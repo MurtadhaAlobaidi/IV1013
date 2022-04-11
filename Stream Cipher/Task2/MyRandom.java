2264c5fc717d967fdcf123d10da226991dd165ae
Murtadha Alobaidi <mhaao@kth.se>
2022-04-11 11:39:35 +0200

    Add files via upload

--- Stream Cipher/Task2/MyRandom.java
+++ Stream Cipher/Task2/MyRandom.java
@@ -1,23 +1,23 @@
 import java.util.Random;
 
 public class MyRandom extends Random {
-        int a = 16807;//7^5
-        int b = 12345678;//
-        int m = 2147483647;//2^(31) - 1
-        long seed = 0;
+    // Task 2
+    public int a = 48271;// 48271 C++11's minstd_rand[22]
+    public int b = 12345678;//
+    public int m = 2147483647;// 2^(31) - 1
+    public long seed = 0;
 
-        public MyRandom(long seed) {
-            setSeed(seed);
-        }
+    public MyRandom(long seed) {
+        this.setSeed(seed);
+    }
 
-        protected int next(int bits) {
-            //from lecture Symmetric Key Encryption I
-            int x0 = (int) ((a * seed + b) % m);
-            seed = x0;
-            return x0;
-        }
+    public int next(int bits) {
+        int x0 = ((int) ((a * seed + b) % m));
+        this.seed = x0;
+        return x0;
+    }
 
-        public synchronized void setSeed(long seed) {
-            this.seed = seed;
-        }
+    public void setSeed(long seed) {
+        this.seed = seed;
+    }
 }
