5b993faa6d6c919a01b3e21f552b33e97b075e81
Murtadha Alobaidi <mhaao@kth.se>
2022-04-17 15:50:56 +0200

    Add files via upload

--- One-way-hash/CollisionResistance.java
+++ One-way-hash/CollisionResistance.java
@@ -7,8 +7,8 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class CollisionResistance {
-    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         System.out.println("\nENTER YOUR MESSAGE TO DIGEST:");
         Scanner scanner = new Scanner(System.in);
         String inputMessage = scanner.nextLine();
@@ -19,7 +19,6 @@ public class CollisionResistance {
     }
 
     public static byte[] messageDigest(String inputDigest) {
-
         byte[] toDigest = new byte[0];
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -49,31 +48,28 @@ public class CollisionResistance {
             md.update(forceBits);
             compareToDigest = md.digest();
             counter++;
-
             if (digest[0] == compareToDigest[0] && digest[1] == compareToDigest[1] && digest[2] == compareToDigest[2]) {
-
                 System.out.println("\n\nIt take " + "[" + counter + "]" + " trials to generate a digest");
                 System.out.print("\nORIGINAL DIGEST :       0x");
-
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < 3; i++)
                     System.out.format("%02x", digest[i] & 0xff);
-                    System.out.print("\nBRUTE-FORCED DIGEST:    0x");
-                }
-                for (int i = 0; i < 3; i++) {
+                System.out.print("\nBRUTE-FORCED DIGEST:    0x");
+                for (int i = 0; i < 3; i++)
                     System.out.format("%02x", compareToDigest[i] & 0xff);
-                }
                 System.out.println("\n");
                 System.out.println("\n");
                 return;
             }
         }
     }
-
+    
     static byte[] ToByteArray(long counter) throws IOException {
         ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
         new DataOutputStream(outputArray).writeLong(counter);
         new DataOutputStream(outputArray).flush();
         return outputArray.toByteArray();
     }
+
 }
+
 
